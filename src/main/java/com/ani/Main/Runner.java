b74420efb1d2f6d593835cb96b70891250b33a76
aniket <007aniketkumar@gmail.com>
2019-07-04 13:46:16 +0530

    code logic committed

---
+++ src/main/java/com/ani/Main/Runner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ani.Main;
+
+/**
+ * @author aniket
+ *
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("This is the runner class");
+	}
+
+}
